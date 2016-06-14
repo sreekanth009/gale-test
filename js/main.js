63b7b270f20769b0be1f122f8fc7a735c19daae9
Jones V Rajan <Jones V Rajan>
2016-06-14 19:43:24 +0530

    Content area refactor

--- js/main.js
+++ js/main.js
@@ -5,21 +5,8 @@ $(window).load(function(){
     controlNav: false,
     animationLoop: false,
     slideshow: false,
-    itemWidth: 160,
-    itemMargin: 40,
-    asNavFor: '#slider'
-  });
-
-  $('#slider').flexslider({
-    animation: "fade",
-    controlNav: false,
-    animationLoop: false,
-    slideshow: false,
-    directionNav: false,
-    sync: "#carousel",
-    start: function(slider){
-      $('body').removeClass('loading');
-    }
+    itemWidth: 130,
+    itemMargin: 50,
   });
 
   $(".image-carousel li span").hover(
@@ -34,5 +21,29 @@ $(window).load(function(){
          height: '38px'
       },'fast');
     });
+
+  //Setting background image, class and blockquotes on load
+  var backgroundImage = $('.slides li:first-child a').prev('img').attr('src');
+  $('#content-area').css('background-image', 'url('+backgroundImage+')');
+  $('#content-area .blockquote').append('"'+$('.slides li:first-child a').attr('data-quote')+'"');
+  $('#content-area .blockquote-author').append('- '+ $('.slides li:first-child a').attr('data-author'));
+  $('.slides li:first-child').addClass('flex-active-slide');
+
+  // Changing background images, class and blockquote on click
+  $('.slides li a ').click(function(event){
+    event.preventDefault;
+    $('#content-area').hide();
+    $('#content-area .blockquote').empty();
+    $('#content-area .blockquote-author').empty();
+    $('.slides li').removeClass('flex-active-slide');
+    $(this).closest('li').addClass('flex-active-slide')
+    var blockquote = $(this).attr('data-quote');
+    var author = $(this).attr('data-author');
+    var backgroundImage = $(this).prev('img').attr('src');
+    $('#content-area .blockquote').append('"'+blockquote+'"');
+    $('#content-area .blockquote-author').append('- ' + author);
+    $('#content-area').css('background-image', 'url('+backgroundImage+')');
+    $('#content-area').fadeIn();
+  });
 
 });
